70fcc2c1d1386240e92bcde6215164348803416c
Tom McGuire <tom.mcguire73@googlemail.com>
2022-12-05 09:24:16 +0000

    Message Structs

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+type logMessage struct {
+	time    string
+	date    string
+	message string
+}
+
+type errMessage struct {
+	time    string
+	date    string
+	message string
+	err     string
+}
+
 func main() {
 
 }
